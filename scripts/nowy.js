7bf3cef498421dcb838ebdf5fbe9749d002afa36
Y-Taras <ytaras@gmail.com>
2016-05-16 21:02:55 +0300

    new version of design

---
+++ scripts/nowy.js
@@ -0,0 +1,44 @@
+function resisize(id)
+{
+        var big = '500',small ='280px';
+        if(document.getElementById(id).width == big)
+        {
+                document.getElementById(id).width = small;
+        }
+        else
+        {
+                document.getElementById(id).width = big;
+        }
+}
+$(document).ready(function(){
+
+        var $menu = $("#menu");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("default")
+                           .addClass("fixed transbg")
+                           .fadeIn('fast');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("fixed transbg")
+                           .addClass("default")
+                           .fadeIn('fast');
+                });
+            }
+        });//scroll
+
+        $menu.hover(
+            function(){
+                if( $(this).hasClass('fixed') ){
+                    $(this).removeClass('transbg');
+                }
+            },
+            function(){
+                if( $(this).hasClass('fixed') ){
+                    $(this).addClass('transbg');
+                }
+            });//hover
+    });//jQuery
