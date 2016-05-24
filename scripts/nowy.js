2cd700421da3d9fa622a49e32eecd2182e5e9557
Y-Taras <ytaras@gmail.com>
2016-05-25 00:02:34 +0300

    Added quotes and new backgrounds

--- scripts/nowy.js
+++ scripts/nowy.js
@@ -1,44 +1,43 @@
-function resisize(id)
-{
-        var big = '500',small ='280px';
-        if(document.getElementById(id).width == big)
-        {
-                document.getElementById(id).width = small;
-        }
-        else
-        {
-                document.getElementById(id).width = big;
-        }
+function resisize(id) {
+    var big = '500', small = '280px';
+    if (document.getElementById(id).width == big) {
+        document.getElementById(id).width = small;
+    }
+    else {
+        document.getElementById(id).width = big;
+    }
 }
-$(document).ready(function(){
+$(document).ready(
+    function () {
 
         var $menu = $("#menu");
 
-        $(window).scroll(function(){
-            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-                $menu.fadeOut('fast',function(){
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100 && $menu.hasClass("default")) {
+                $menu.fadeOut('fast', function () {
                     $(this).removeClass("default")
-                           .addClass("fixed transbg")
-                           .fadeIn('fast');
+                        .addClass("fixed transbg")
+                        .fadeIn('fast');
                 });
-            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
-                $menu.fadeOut('fast',function(){
+            } else if ($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast', function () {
                     $(this).removeClass("fixed transbg")
-                           .addClass("default")
-                           .fadeIn('fast');
+                        .addClass("default")
+                        .fadeIn('fast');
                 });
             }
         });//scroll
 
         $menu.hover(
-            function(){
-                if( $(this).hasClass('fixed') ){
+            function () {
+                if ($(this).hasClass('fixed')) {
                     $(this).removeClass('transbg');
                 }
             },
-            function(){
-                if( $(this).hasClass('fixed') ){
+            function () {
+                if ($(this).hasClass('fixed')) {
                     $(this).addClass('transbg');
                 }
-            });//hover
-    });//jQuery
+            });//jQuery
+
+    });//hover
