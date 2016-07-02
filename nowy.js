d7dbc88b66ebca6932093b4e1959b63fda93afd5
Y-Taras <ytaras@gmail.com>
2016-07-02 15:28:25 +0300

    New version from Nadvirna team

---
+++ nowy.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+  $('.bxslider').bxSlider({
+      minSlides: 3,
+  maxSlides: 3,
+  slideWidth: 300,
+  slideMargin: 10,
+  speed: 2000,
+ pager:false
+});
+});
--- nowy2.js
+++ nowy2.js
@@ -1,4 +1,3 @@
-    $(document).ready(function(){
   $('.bxslider').bxSlider({
     adaptiveHeight:true,
     minSlides: 3,
@@ -6,13 +5,17 @@
   slideWidth: 300,
   slideMargin: 5,
   speed: 2000,
- pager:false
-});
-});
+ pager:false});
 $('#s1').cycle({ 
     fx:     'toss', 
     speed:  1000, 
     next:   '#s1', 
+    timeout: 0
+});
+  $('#s3').cycle({ 
+    fx:     'toss', 
+    speed:  1000, 
+    next:   '#s3', 
     timeout: 0
 });
 $('#s2').cycle({ 
@@ -21,9 +24,4 @@ $('#s2').cycle({
     next:   '#s2', 
     timeout: 0
 });
-$('#s3').cycle({ 
-    fx:     'fade', 
-    speed:  1000, 
-    next:   '#s3', 
-    timeout: 0
-});
+  });
